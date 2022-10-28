2320ef269283154796e970376b42a664b3bd74e8
LauKwaiFanHK <laukwaifanthbde@gmail.com>
2022-10-28 18:28:36 +0200

    Create initial endpoints

--- index.js
+++ index.js
@@ -1,22 +1,75 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import helmet from "helmet";
 
 // Create an app to start an express server.
 const app = express();
 const port = 3000;
 
+// Enable the app to secure HTTP requests.
+app.use(helmet());
 // Enable the app to create a cookie parser middleware.
 app.use(cookieParser());
+// Enable the app to process POST request.
+app.use(express.json());
 
 // Set a route for root URL.
 // The app responds with a message for requests to the root URL. 
 app.get('/', (req, res) => {
-    res.send('Hello world!!!')
+    // Check if a user is logged in.
+    let username = req.cookies.username;
+    if (username) {
+        res.send(`Hello ${username}!`);
+    } else {
+        res.send('Hello world!!!');
+    }
+});
+
+// Set a route for user to login.
+app.get('/login', (req, res) => {
+    // Check if login failed by checking if the path contains a query parameter with the valu 'failed'.
+    let failedLogin = req.query.status === 'failed' ? true : false;
+
+    if (failedLogin) {
+        res.send('Login failed. Invalid username or password.');
+    } else {
+        res.send('Please enter your username and password to login.');
+    }
+});
+
+app.get('/welcome', (req, res) => {
+    let username = req.cookies.username;
+    res.send(`Welcome back, ${username}!`);
+})
+
+// Set a route to handle POST request for login.
+app.post('/login', (req, res) => {
+    const userDetails = {
+        username: "Billy",
+        password: "abc123",
+    };
+
+    let { username, password } = req.body;
+    if (username === 'Billy' && password === 'abc123') {
+        res.cookie("username", username);
+        //res.send('Login successful.');
+        return res.redirect('/welcome');
+    } else {
+        //res.send('Login failed.');
+        return res.redirect('/login?status=failed')
+    }
+});
+
+// Set a route for user to logout. 
+// Delete the username cookie when user logged out.
+app.get('/logout', (req, res) => {
+    res.clearCookie('username');
+    res.redirect('/login');
 });
 
 // Set a route for sending a cookie from the server to a client.
 app.get('/setCookie', (req, res) => {
-    const cookieName = 'userName';
+    const cookieName = 'username';
     const cookieValue = 'Billy Wonka';
     res.cookie(cookieName, cookieValue, {
         maxAge: 24 * 60 * 60 * 1000,
@@ -34,7 +87,7 @@ app.get('/getCookie', (req, res) => {
 
 // Set a route to delete saved cookies
 app.get('/deleteCookie', (req, res) => {
-    res.clearCookie('userName');
+    res.clearCookie('username');
     res.send('Cookies deleted');
 });
 
